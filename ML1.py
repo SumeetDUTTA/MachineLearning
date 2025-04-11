2a0293bcc05f2870d6416e42c6533842b38cb954
Sumeet <sumeetdutta040@gmail.com>
2025-04-11 21:45:46 +0530

    1st Commit
    Signed-off-by: Sumeet <sumeetdutta040@gmail.com>

--- ML1.py
+++ ML1.py
@@ -5,12 +5,12 @@ from sklearn.metrics import mean_squared_error
 
 diabetes = datasets.load_diabetes()
 
-diabetes_X = diabetes.data
-diabetes_X_train = diabetes_X[:-30]
-diabetes_X_test = diabetes_X[-30:]
+diabetes_X = diabetes.data[:, np.newaxis, 2]
+diabetes_X_train = diabetes_X[:-20]
+diabetes_X_test = diabetes_X[-20:]
 
-diabetes_y_train = diabetes.target[:-30]
-diabetes_y_test = diabetes.target[-30:]
+diabetes_y_train = diabetes.target[:-20]
+diabetes_y_test = diabetes.target[-20:]
 
 model = linear_model.LinearRegression()
 model.fit(diabetes_X_train, diabetes_y_train)
@@ -22,7 +22,7 @@ print(f"Mean squared error: {mean_squared_error(diabetes_y_test, diabetes_y_pred
 print(f"Weight: {model.coef_}")
 print(f"Intercept: {model.intercept_}")
 
-# plt.scatter(diabetes_X_test, diabetes_y_test,  color='black')
-# plt.plot(diabetes_X_test, diabetes_y_predict, color='blue', linewidth=3)
-#
-# plt.show()
+plt.scatter(diabetes_X_test, diabetes_y_test,  color='black')
+plt.plot(diabetes_X_test, diabetes_y_predict, color='blue', linewidth=3)
+
+plt.show()
